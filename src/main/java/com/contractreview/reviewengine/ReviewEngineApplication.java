1dba71c9c3eb3860ecc964e5b65ab792156a04a2
Saltysth <1432488520@qq.com>
2025-10-14 17:56:11 +0800

    修复bean注入

--- src/main/java/com/contractreview/reviewengine/ReviewEngineApplication.java
+++ src/main/java/com/contractreview/reviewengine/ReviewEngineApplication.java
@@ -2,20 +2,26 @@ package com.contractreview.reviewengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 审查引擎应用启动类
- * 
+ *
  * @author SaltyFish
  */
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
 @EnableFeignClients(basePackages = "com.contract.common.feign")
+@EnableJpaRepositories(basePackages = "com.contractreview.reviewengine.infrastructure.persistence.repository")
+@EntityScan(basePackages = "com.contractreview.reviewengine.infrastructure.persistence.entity")
 @EnableTransactionManagement
 public class ReviewEngineApplication {
 
